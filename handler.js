f57eda5b517d0df77d69d4efa521040b3e963732
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-09-23 22:10:07 +0000

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,10 +1,4 @@
-/**
- * Serialize Message
- * Created By ABZTECH
- * Follow https://github.com/abrahamdw882
- * Whatsapp : https://whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c
- */
-
+// ./handler.js
 const { downloadMediaMessage } = require('@whiskeysockets/baileys');
 
 async function serializeMessage(sock, msg) {
@@ -30,7 +24,9 @@ async function serializeMessage(sock, msg) {
     const mediaType = type.replace('Message', '').toLowerCase();
     const mimetype = msg.message?.[type]?.mimetype || null;
 
-    const groupMetadata = isGroup ? await sock.groupMetadata(from).catch(() => {}) : '';
+    const groupMetadata = isGroup
+        ? await sock.groupMetadata(from).catch(() => undefined)
+        : undefined;
 
     let quoted;
     const ctxInfo = msg.message?.extendedTextMessage?.contextInfo;
@@ -64,12 +60,12 @@ async function serializeMessage(sock, msg) {
         mediaType,
         mimetype,
         quoted,
-        reply: async (text, options={}) => await sock.sendMessage(from, { text, ...options }, { quoted: msg }),
-        send: async (content, options={}) => await sock.sendMessage(from, typeof content === 'string' ? { text: content, ...options } : content, { quoted: msg }),
+        reply: async (text, options={}) => await sock.sendMessage(from,{text,...options},{quoted:msg}),
+        send: async (content, options={}) => await sock.sendMessage(from, typeof content==='string'?{text:content,...options}:content, {quoted:msg}),
         react: async emoji => await sock.sendMessage(from, { react: { text: emoji, key: msg.key } }),
         forward: async (jid, force=false) => await sock.sendMessage(jid, { forward: msg, force }),
-        download: async () => isMedia ? await downloadMediaMessage(msg, 'buffer', {}, sock) : (quoted?.isMedia ? await quoted.download() : null)
+        download: async () => isMedia ? await downloadMediaMessage(msg,'buffer',{},sock) : (quoted?.isMedia ? await quoted.download() : null)
     };
 }
 
-module.exports = serializeMessage;
+module.exports = { serializeMessage };
