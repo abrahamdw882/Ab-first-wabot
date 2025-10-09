d8b3fa905fa0bbc7163d8173aa7a0a37632ae514
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-10-09 06:24:01 +0000

    Update mention.js

--- plugins/mention.js
+++ plugins/mention.js
@@ -17,7 +17,7 @@ module.exports = {
 
             const name = m.pushName || m.sender.split('@')[0];
             const audioUrl = 'https://files.catbox.moe/t1zkqo.mp3';
-            const thumbnail = 'https://i.ibb.co/rfsYsGn1/Ayanokouji-1.jpg';
+            const thumbnail = 'https://i.ibb.co/65fwTVG/carbon-3.png';
             const quoted = {
                 key: {
                     fromMe: false,
